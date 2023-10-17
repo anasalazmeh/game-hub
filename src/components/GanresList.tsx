b3517f45dc0c8bf558398d8f8f89d51e136fa1dd
anasalazmeh <anoosalazmeh@gmail.com>
2023-10-17 22:55:29 +0300

    show a skeleton while fetching the genres

--- src/components/GanresList.tsx
+++ src/components/GanresList.tsx
@@ -1,19 +1,25 @@
 import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
 import UseGenres from '../hooks/UseGenres'
 import IamgeSize from '../services/image-url'
+import GenresListSkeleton from './GenresListSkeleton'
 
 
 const GanresList = () => {
-   const{data}= UseGenres()
+   const{data,isloading}= UseGenres()
+   const Skeleton=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
   return (
-    <List >
-          {data?.map(item => <ListItem paddingY={2} key={item.id}>
-            <HStack>
-                  <Image boxSize='32px' borderRadius={5} src={IamgeSize(item.image_background)}/>
-                  <Text fontSize='lg' marginLeft={1}>{item.name}</Text>
-            </HStack>
-            </ListItem>)}
-    </List>
+    
+    <>
+    {isloading && Skeleton.map(i=> <GenresListSkeleton key={i}/>)}
+          <List >
+              {data?.map(item => <ListItem paddingY={2} key={item.id}>
+                  <HStack>
+                      <Image boxSize='32px' borderRadius={5} src={IamgeSize(item.image_background)} />
+                      <Text fontSize='lg' marginLeft={1}>{item.name}</Text>
+                  </HStack>
+              </ListItem>)}
+          </List>
+    </>
   )
 }
 
---
+++ src/components/GenresListSkeleton.tsx
@@ -0,0 +1,16 @@
+import { HStack, List, ListItem, Skeleton, SkeletonText } from '@chakra-ui/react'
+import React from 'react'
+
+const GenresListSkeleton = () => {
+  return (
+      <List paddingY={2}>
+          <ListItem display='flex' justifyContent='space-between'>
+                        <Skeleton boxSize='32px' >
+                        </Skeleton>
+            
+              </ListItem>
+        </List>
+  )
+}
+
+export default GenresListSkeleton
