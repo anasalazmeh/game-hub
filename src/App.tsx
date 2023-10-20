ec538e38204a045d71c386f152f72dcde83ef945
anasalazmeh <anoosalazmeh@gmail.com>
2023-10-20 22:36:39 +0300

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,20 @@ import { Genres } from './hooks/UseGenres'
 import PlatformSelector from './components/PlatformSelector'
 import { Platform } from './hooks/UseGame'
 import SortSelector from './components/SortSelector'
+import GameHeading from './components/GameHeading'
 export interface GameQuery {
   genres:number,
   platforms:number,
   ordering:string,
   search:string
 }
+export interface GamesHeading {
+  genres:Genres,
+  platforms:Platform,
+}
 function App() {
   const [GameQuery,setGameQuery]=useState<GameQuery>({}as GameQuery)
+  const [gameheading, setGameHeading] = useState<GamesHeading>({} as GamesHeading)
   return (
     <Grid templateAreas={
       {
@@ -32,17 +38,26 @@ function App() {
       </GridItem>
       <Show above='lg'>
         <GridItem area='Aside' paddingX={2} >
-          <GanresList selectgenres={(genres) => setGameQuery({ ...GameQuery, genres: genres.id })}></GanresList>
+          <GanresList selectgenres={(genres) =>
+             {
+            setGameQuery({ ...GameQuery, genres: genres.id }) ;
+            setGameHeading({ ...gameheading, genres: genres })
+              }}></GanresList>
         </GridItem>
       </Show>
       <GridItem area='Main' >
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-            
-        <PlatformSelector  onSelectPlatform={(Platform) => setGameQuery({ ...GameQuery, platforms: Platform.id })}/>
-          </Box>
-          <SortSelector onSortSelector={(sort) => setGameQuery({ ...GameQuery,ordering:sort })} />
-        </Flex>
+        <Box paddingLeft={2}>
+          <GameHeading gameHeading={gameheading}/>
+          <Flex  marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector onSelectPlatform={(Platform) =>{
+                 setGameQuery({ ...GameQuery, platforms: Platform.id });
+                setGameHeading({ ...gameheading, platforms: Platform })
+              }} />
+            </Box>
+            <SortSelector onSortSelector={(sort) => setGameQuery({ ...GameQuery, ordering: sort })} />
+          </Flex>
+        </Box>
         <GameGrid gameQuery={GameQuery}></GameGrid>
       </GridItem>
     </Grid>
---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,14 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+import { GamesHeading } from '../App'
+interface Porps{
+    gameHeading :GamesHeading
+}
+const GameHeading = ({ gameHeading }:Porps) => {
+    const heading=`${gameHeading.platforms?.name||''} ${gameHeading.genres?.name || ""} Game`;
+  return (
+      <Heading  fontSize='5xl' marginY={5}>{heading}</Heading>
+  )
+}
+
+export default GameHeading
