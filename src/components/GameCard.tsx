29951656a6e3f034e3048cfc8a4a94147a2a099a
anasalazmeh <anoosalazmeh@gmail.com>
2023-10-21 00:11:13 +0300

    edit color moda

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Game } from '../hooks/UseGame'
-import { Card, CardBody, CardHeader, HStack, Heading, Image, Text } from '@chakra-ui/react'
+import { Card, CardBody, CardHeader, HStack, Heading, Image, Text, useColorMode } from '@chakra-ui/react'
 import Platformsicon from './Platformsicon'
 import CirticScora from './CirticScora'
 import IamgeSize from '../services/image-url'
@@ -9,12 +9,14 @@ interface Pops {
     game: Game
 }
 const GameCard = ({ game }: Pops) => {
+
     return (
         <Card >
             <Image src={IamgeSize(game.background_image)} ></Image>
             <CardBody>
                 <HStack justifyContent={'space-between'} marginBottom={2}>
-                    <Platformsicon platform={game.parent_platforms.map((p) => p.platform)}></Platformsicon>
+                    <Platformsicon
+                     platform={game.parent_platforms.map((p) => p.platform)}></Platformsicon>
                     <CirticScora scora={game.metacritic}></CirticScora>
                 </HStack>
                 <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GanresList.tsx
+++ src/components/GanresList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Heading, Image, List, ListItem, Text, background } from '@chakra-ui/react'
+import { Button, HStack, Heading, Image, List, ListItem, Text, background, useColorMode } from '@chakra-ui/react'
 import UseGenres, { Genres } from '../hooks/UseGenres'
 import IamgeSize from '../services/image-url'
 import GenresListSkeleton from './GenresListSkeleton'
@@ -8,9 +8,15 @@ interface Porps{
     selectgenres:(genres:Genres)=>void
 }
 const GanresList = ({ selectgenres }:Porps) => {
+    const { toggleColorMode, colorMode } = useColorMode();
     const { data, isloading } = UseGenres()
     const [ative,setative]=useState(0)
    const Skeleton=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    let colorItem ='gray.100'
+   if(colorMode=='dark'){
+       colorItem ='gray.700'
+   }
+
   return (
     <>
     <Heading marginLeft={3} fontSize='2xl' marginBottom={2}>Genres</Heading>
@@ -18,7 +24,12 @@ const GanresList = ({ selectgenres }:Porps) => {
     
           <List >
               {data?.map(genres => <ListItem paddingY={2} key={genres.id} >
-                  <HStack transition='all 0.3s' onClick={() => { selectgenres(genres); setative(genres.id) }} background={ative == genres.id ? 'gray.700' :''} width='100%' cursor='pointer' paddingX={3} _hover={{ background:'gray.700'} } borderRadius={2} paddingY={2}>
+                  <HStack 
+                    transition='all 0.3s'
+                     onClick={() => { selectgenres(genres); setative(genres.id) }} 
+                      background={ative == genres.id ? colorItem : ''} width='100%' cursor='pointer' paddingX={3}
+                    
+                      _hover={{ background :colorItem}} borderRadius={2} paddingY={2}>
                           <Image objectFit='cover' boxSize='32px' borderRadius={5} src={IamgeSize(genres.image_background)} />
                       <Text variant='link' fontWeight={ative == genres.id?'bold':'normal'} fontSize='lg' marginLeft={1}>{genres.name}</Text>
                       </HStack>
