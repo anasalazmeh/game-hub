ad31e1160a20b92ab965ba518ee26658db41a954
anasalazmeh <anoosalazmeh@gmail.com>
2023-10-20 22:45:36 +0300

    Clean up the genres

--- src/components/GanresList.tsx
+++ src/components/GanresList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Text, background } from '@chakra-ui/react'
+import { Button, HStack, Heading, Image, List, ListItem, Text, background } from '@chakra-ui/react'
 import UseGenres, { Genres } from '../hooks/UseGenres'
 import IamgeSize from '../services/image-url'
 import GenresListSkeleton from './GenresListSkeleton'
@@ -13,11 +13,13 @@ const GanresList = ({ selectgenres }:Porps) => {
    const Skeleton=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
   return (
     <>
+    <Heading marginLeft={3} fontSize='2xl' marginBottom={2}>Genres</Heading>
     {isloading && Skeleton.map(i=> <GenresListSkeleton key={i}/>)}
+    
           <List >
               {data?.map(genres => <ListItem paddingY={2} key={genres.id} >
                   <HStack transition='all 0.3s' onClick={() => { selectgenres(genres); setative(genres.id) }} background={ative == genres.id ? '#4A5568;':''} width='100%' cursor='pointer' paddingX={3} _hover={{ background:"#4A5568;"} } borderRadius={2} paddingY={2}>
-                          <Image boxSize='32px' borderRadius={5} src={IamgeSize(genres.image_background)} />
+                          <Image objectFit='cover' boxSize='32px' borderRadius={5} src={IamgeSize(genres.image_background)} />
                       <Text variant='link' fontWeight={ative == genres.id?'bold':'normal'} fontSize='lg' marginLeft={1}>{genres.name}</Text>
                       </HStack>
               </ListItem>)}
