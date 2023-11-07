1857332da22ffc1cfa540a61155df22d96e2c132
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-07 17:28:45 +0300

    update error in ExpandableText.tsx

--- src/hooks/UseGame.ts
+++ src/hooks/UseGame.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 
 import ms from "ms";
 import StoreQuery from "../Zustand/store";
-import { Game } from "../entities/Game";
+import  Game  from "../entities/Game";
 import APICLIENT, { FetchRespones } from "../services/API-client";
 
 const Apiclient = new APICLIENT<Game>("/games");
--- src/hooks/UseGameDetail.ts
+++ src/hooks/UseGameDetail.ts
@@ -4,7 +4,7 @@ import  Platform  from "../entities/Platform";
 import APICLIENT from "../services/API-client";
 import  Game  from "../entities/Game";
 
-const UseGameDetail = (id?:number |string) => {
+const UseGameDetail = (id:number |string) => {
     const Apiclient=new APICLIENT<Game>(`/games/${id}`)
     return useQuery<Game>({
         queryKey:[`game :${id}`],
